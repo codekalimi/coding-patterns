04c2d138c58b83a6206bca3e50496ffd498adbb7
Aamir Kalimi <aamir_k@ormae.com>
2023-02-25 14:39:26 +0530

    find current_maximum_value

--- sliding_window/current_maximum_value.py
+++ sliding_window/current_maximum_value.py
@@ -15,3 +15,6 @@ if __name__ == '__main__':
     arr = [-4, 2, -5, 3, 6]
     result = find_current_maximum(arr, 3)
     print(result)
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    result = find_current_maximum(arr, 4)
+    print(result)
