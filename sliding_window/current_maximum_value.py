e33627b90dcebe12d127306b95e335559de109d4
Aamir Kalimi <aamir_k@ormae.com>
2023-02-25 14:37:12 +0530

    find current_maximum_value

--- sliding_window/current_maximum_value.py
+++ sliding_window/current_maximum_value.py
@@ -1,20 +1,17 @@
 def find_current_maximum(elements, w):
     n = len(elements)
-    start = 0
-    result = []
+    maximumElements = []
     maximum = 0
     for i in range(n):
-        temp = elements[start]
+        temp = arr[i]
+        if temp > maximum:
+            maximum = temp
         if i >= w - 1:
-            if temp > maximum:
-                maximum = temp
-                result.append(maximum)
-                start = start + 1
-            else:
-                start = start + 1
-    print(result)
+            maximumElements.append(maximum)
+    return maximumElements
 
 
 if __name__ == '__main__':
-    arr = [-4, 5, 4, -4, 4, 6, 7]
-    find_current_maximum(arr, 3)
+    arr = [-4, 2, -5, 3, 6]
+    result = find_current_maximum(arr, 3)
+    print(result)
